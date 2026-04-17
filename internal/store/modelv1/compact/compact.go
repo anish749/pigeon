7259f7b7ef00b4f633f702320f58f349c7554239
Anish C <anish749@gmail.com>
2026-04-18 00:58:00 +0200

    compact: fix thread compaction to apply raw from edits, add tests

--- internal/store/modelv1/compact/compact.go
+++ internal/store/modelv1/compact/compact.go
@@ -88,6 +88,7 @@ func CompactThread(f *modelv1.ThreadFile) *modelv1.ThreadFile {
 		if e, ok := latestEdits[t.msg.ID]; ok {
 			deduped[i].msg.Text = e.Text
 			deduped[i].msg.Attachments = cloneAttachments(e.Attachments)
+			deduped[i].msg.Raw = e.Raw
 		}
 	}
 
